82528a8a736bfca197a52b969060d3b1676ab2d9
Sobhan Bera <sobhanbera258@gmail.com>
2022-01-24 20:45:32 +0530

    landing page implemented fully

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
 import type { NextPage } from 'next'
 
-import styles from '../styles/main/index.module.scss'
+import styles from '../styles/pages/main.module.scss'
 
 const Home: NextPage = () => {
 	return (
-		<div className={styles.test}>HJK</div>
+		<div className={styles.mainPage}>
+			<div className={styles.greeting}>
+				<h2>Hello There,</h2>
+				<h2>I am Sobhan Bera</h2>
+			</div>
+
+			<div className={styles.textFlipper}>
+				<ul className={styles.flipper}>
+					<li>Android Developer</li>
+					<li>Web Developer</li>
+					<li>Student</li>
+					<li>Quick Learner</li>
+					<li>Linux/Neovim User</li>
+				</ul>
+			</div>
+		</div>
 	)
 }
 
 export default Home
+
